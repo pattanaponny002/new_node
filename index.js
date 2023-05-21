cd0df430d21522b35db37b5512aca7eef0be5bc2
Ponny <oloponnyolo@gmail.com>
2023-05-21 23:59:22 +0800

    newnode

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const port = 4001;
 app.get("/", (req, res) => {
   res.send("hello");
 });
+app.get("/test", (req, res) => {
+  res.send("test");
+});
 app.listen(port, () => console.log("server is running", port));

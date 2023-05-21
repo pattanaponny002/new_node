efec2ff99c9c5e18d524c133d249db4c0ef7cb8d
Ponny <oloponnyolo@gmail.com>
2023-05-21 23:45:30 +0800

    bless

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-const port = 4000;
+const port = 4001;
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.listen(port, () => console.log("server is running"));
+app.listen(port, () => console.log("server is running", port));

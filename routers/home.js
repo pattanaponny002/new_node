60f3f81a31c3311b96da82650ae65d696e72f78a
Ponny <oloponnyolo@gmail.com>
2023-05-22 20:27:14 +0800

    fixing_mongoose

--- routers/home.js
+++ routers/home.js
@@ -15,11 +15,18 @@ router.get("/", (req, res) => {
 router.get("/test", (req, res) => {
   res.json({ test: "home", master: 66 });
 });
+
+///http://localhost:4001/home/api/add work perfectly find.!!
+///https://new-node-fp4am1plv-pattanaponny002.vercel.app/home/api/add
+///504: GATEWAY_TIMEOUT
+// Code: FUNCTION_INVOCATION_TIMEOUT
+// ID: hkg1::p5zfx-1684758148349-b1b8f3952e1f
 router.get("/add", async (req, res) => {
   const result = await UserNew.create({
     username: "testHomeSuccess",
     password: "testHomeSuccess",
   });
+  console.log("adding");
 
   res.json({ test: "home", result });
 });

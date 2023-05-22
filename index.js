1481b2cfd22c934f60d57cec566f2f8f0a7a35ac
Ponny <oloponnyolo@gmail.com>
2023-05-22 20:51:45 +0800

    port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const home_api = require("./routers/home");
 const town_api = require("./routers/town");
 const app = express();
+const dotenv = require("dotenv");
 
-const port = 4001;
-
+dotenv.config();
 app.use("/home/api", home_api);
 app.use("/town/api", town_api);
 
-app.listen(port, () => console.log("server is running", port));
+app.listen(process.env.PORT || 4001, () =>
+  console.log("server is running", process.env.PORT)
+);
